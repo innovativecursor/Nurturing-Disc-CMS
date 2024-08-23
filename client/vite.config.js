f101bf196bfe9985705c2da00eef96fd2ca7a21b
Shawn Almeida <shawn9920@gmail.com>
2024-08-23 19:08:19 +0530

    made server level changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -15,8 +15,8 @@ export default defineConfig({
   },
   define: {
     "process.env": {
-      REACT_APP_UAT_URL: "http://localhost:8080",
-      // REACT_APP_UAT_URL: "http://170.187.250.246:8080",
+      // REACT_APP_UAT_URL: "http://localhost:8080",
+      REACT_APP_UAT_URL: "http://170.187.250.246:8080",
       REACT_APP_ENCRYPTION: "WABBALABBA@3344$$1DUB43DUB",
     },
   },
